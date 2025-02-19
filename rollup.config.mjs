ee18a96837a358f3af0d122851911b329913cb68
Ahmed Alsanadi <ahmed11alsanadi11@gmail.com>
2025-02-19 12:15:44 +0300

    fix(node): version to resolve ci issues

--- rollup.config.mjs
+++ rollup.config.mjs
@@ -1,10 +1,12 @@
-// rollup.config.mjs
 import resolve from '@rollup/plugin-node-resolve';
 import typescript from '@rollup/plugin-typescript';
 import commonjs from '@rollup/plugin-commonjs';
 import dts from 'rollup-plugin-dts';
 import postcss from 'rollup-plugin-postcss';
-import packageJson from './package.json' assert { type: 'json' };
+import { createRequire } from 'module';
+
+const require = createRequire(import.meta.url);
+const packageJson = require('./package.json');
 
 export default [
     {

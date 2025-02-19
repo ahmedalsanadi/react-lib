5d1fdfb3b663b3a13dc7affeb9f78eb63a2cbd29
Ahmed Alsanadi <ahmed11alsanadi11@gmail.com>
2025-02-19 16:34:25 +0300

    fix(node): problem to solve circle ci issue

--- rollup.config.mjs
+++ rollup.config.mjs
@@ -1,12 +1,10 @@
+// rollup.config.mjs
 import resolve from '@rollup/plugin-node-resolve';
 import typescript from '@rollup/plugin-typescript';
 import commonjs from '@rollup/plugin-commonjs';
 import dts from 'rollup-plugin-dts';
 import postcss from 'rollup-plugin-postcss';
-import { createRequire } from 'module';
-
-const require = createRequire(import.meta.url);
-const packageJson = require('./package.json');
+import packageJson from './package.json' assert { type: 'json' };
 
 export default [
     {
